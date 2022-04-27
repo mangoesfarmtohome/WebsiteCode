f565e72b5c17577fd951c3bfa47af15bb7b9edbf
AbdulKareem <as.abdulkareem@gmail.com>
2022-04-28 01:46:28 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import Mango from './Mango.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Mango} className="App-logo" alt="logo" />
+        <h1>Fresh Mangoes Directly From Farm to home</h1>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
